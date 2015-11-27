f4ae2963d359b1565dee3d35601668cc8ba32dc3
Scott Ashmore <dev.scottashmore@gmail.com>
2015-11-27 17:54:26 +0000

    updates

--- server/routes/views/index.js
+++ server/routes/views/index.js
@@ -2,14 +2,13 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
 	
-	var view = new keystone.View(req, res);
+	var view   = new keystone.View(req, res);
 	var locals = res.locals;
 	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
 	locals.section = 'home';
+
+	view.query('page', keystone.list('page').model.find({slug: 'home'}));
 	
-	// Render the view
 	view.render('index');
 	
 };
