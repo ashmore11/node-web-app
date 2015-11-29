fb12fe253b3698034337435b8243e3e3afd12cd1
Scott Ashmore <sashmore@hi-res.net>
2015-11-29 16:38:32 +0000

    updates

--- makefile
+++ makefile
@@ -1,9 +1,6 @@
 setup:
 	npm install
 
-styles_watch:
-	stylus -w client/styles/app.styl -o public/css/
-
 watch:
 	NODE_ENV=development gulp
 
--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -37,10 +37,14 @@ exports.initLocals = function(req, res, next) {
 exports.requireUser = function(req, res, next) {
 	
 	if (!req.user) {
+
 		req.flash('error', 'Please sign in to access this page.');
 		res.redirect('/keystone/signin');
+
 	} else {
+
 		next();
+		
 	}
 	
 };
